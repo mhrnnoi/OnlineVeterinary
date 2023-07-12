4c5438ebf1afaf5e6f1387d979bec1a78ceebefa
mehran noeiaghdam <mehrannois20@gmail.com>
2023-07-12 16:33:35 +0330

    first step in unit test

---
+++ OnlineVeterinary/Content/tests/UnitTests/OnlineVeterinary.Application.UnitTests/Auth/Commands/ChangeEmailCommandHandlerTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineVeterinary.Application.UnitTests.Auth.Commands
+{
+    public class ChangeEmailCommandHandlerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
